2d64a149e143c5bfe226a7748afe16a6709f0580
maskudo <manujkarki101@gmail.com>
2022-10-20 15:56:51 +0545

    remap buffer nav

--- dotfiles/nvim/.config/nvim/lua/mk489/remap.lua
+++ dotfiles/nvim/.config/nvim/lua/mk489/remap.lua
@@ -32,8 +32,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-h>", ":bnext<CR>", opts)
-keymap("n", "<S-l>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 
 --telescope remaps
