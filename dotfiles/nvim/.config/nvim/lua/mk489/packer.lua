ceafb0212f22d161c573e6c3b61ccf57417e4bd8
maskudo <manujkarki101@gmail.com>
2022-11-02 21:36:23 +0545

    add rust tools

--- dotfiles/nvim/.config/nvim/lua/mk489/packer.lua
+++ dotfiles/nvim/.config/nvim/lua/mk489/packer.lua
@@ -138,6 +138,9 @@ return packer.startup(function(use)
   use 'lewis6991/impatient.nvim'
 
   use 'ggandor/leap.nvim'
+
+  use 'simrat39/rust-tools.nvim'
+  use 'rust-lang/rust.vim'
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
