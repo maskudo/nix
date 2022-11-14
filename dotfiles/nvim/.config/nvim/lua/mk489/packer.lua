1076036031a43158481de03af8782bbb59dbfd51
maskudo <manujkarki101@gmail.com>
2022-11-14 21:03:07 +0545

    add null-ls

--- dotfiles/nvim/.config/nvim/lua/mk489/packer.lua
+++ dotfiles/nvim/.config/nvim/lua/mk489/packer.lua
@@ -146,6 +146,8 @@ return packer.startup(function(use)
   use 'rust-lang/rust.vim'
 
   use "ahmedkhalf/project.nvim"
+
+  use "jose-elias-alvarez/null-ls.nvim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
