f4a6209f9dd3321647be019d4b3d5264b8d01381
maskudo <manujkarki101@gmail.com>
2023-04-26 22:14:27 +0545

    add prettier and tailwind support and minor changes

--- dotfiles/lazyvim/.config/nvim/lua/plugins/disabled.lua
+++ dotfiles/lazyvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "folke/noice.nvim", enabled = false },
   { "ggandor/flit.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
 }
---
+++ dotfiles/lazyvim/.config/nvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,19 @@
+return {
+  "rebelot/kanagawa.nvim",
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+    },
+  },
+}
