640992a2c2ca16ee490d19d0c9efdd3b7fe4543f
maskudo <manujkarki101@gmail.com>
2023-05-17 21:24:50 +0545

    add bashrc and small changes

--- dotfiles/lazyvim/.config/nvim/lua/plugins/disabled.lua
+++ dotfiles/lazyvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   { "folke/noice.nvim", enabled = false },
   { "ggandor/flit.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
+  -- { "rcarriga/nvim-notify", enabled = false },
 }
