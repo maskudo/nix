70ac827e780ed010956ce3b45fcbad6c291940c2
maskudo <manujkarki101@gmail.com>
2024-01-08 22:22:32 +0545

    feat: colorize tailwindcss colors

--- dotfiles/nvim/.config/nvim/lua/plugins/misc.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,67 +1,92 @@
 -- Miscelaneous fun stuff
 return {
-  {
-    "echasnovski/mini.comment",
-    event = "BufEnter",
-    opts = {
-      options = {
-        custom_commentstring = function()
-          return require("ts_context_commentstring.internal").calculate_commentstring()
-              or vim.bo.commentstring
-        end,
-      },
-    },
-    dependencies = {
-      {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        lazy = true,
-        opts = {
-          enable_autocmd = false,
-        },
-      },
-    },
-  },
-  {
-    "echasnovski/mini.pairs",
-    version = "*",
-    event = "VeryLazy",
-    config = function()
-      require("mini.pairs").setup()
-    end,
-  },
-  {
-    "kazhala/close-buffers.nvim",
-    event = "VeryLazy",
-    opts = {
-      preserve_window_layout = { "this", "nameless" },
-    },
-  },
-  { "tpope/vim-surround", event = "VeryLazy" }, -- Surround stuff with the ys-, cs-, ds- commands
-  {
-    "NvChad/nvim-colorizer.lua",
-    event = "VeryLazy",
-    config = function()
-      require("colorizer").setup({
-        filetypes = { "*" },
-        buftypes = {},
-      })
-    end,
-  },
-  {
-    "folke/flash.nvim",
-    -- event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {
-      modes = {
-        search = {
-          enabled = false,
-        },
-        char = {
-          enabled = false,
-        },
-      },
-    },
-    lazy = true,
+	{
+		"echasnovski/mini.comment",
+		event = "BufEnter",
+		opts = {
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring()
+						or vim.bo.commentstring
+				end,
+			},
+		},
+		dependencies = {
+			{
+				"JoosepAlviste/nvim-ts-context-commentstring",
+				lazy = true,
+				opts = {
+					enable_autocmd = false,
+				},
+			},
+		},
+	},
+	{
+		"echasnovski/mini.pairs",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			require("mini.pairs").setup()
+		end,
+	},
+	{
+		"kazhala/close-buffers.nvim",
+		event = "VeryLazy",
+		opts = {
+			preserve_window_layout = { "this", "nameless" },
+		},
+	},
+	{ "tpope/vim-surround", event = "VeryLazy" }, -- Surround stuff with the ys-, cs-, ds- commands
+	{
+		"NvChad/nvim-colorizer.lua",
+		event = "VeryLazy",
+		config = function()
+			require("colorizer").setup({
+				filetypes = {
+					"*",
+					cmp_docs = { always_update = true },
+				},
+				buftypes = {},
+				user_default_options = {
+					RGB = true, -- #RGB hex codes
+					RRGGBB = true, -- #RRGGBB hex codes
+					names = true, -- "Name" codes like Blue or blue
+					RRGGBBAA = false, -- #RRGGBBAA hex codes
+					AARRGGBB = false, -- 0xAARRGGBB hex codes
+					rgb_fn = false, -- CSS rgb() and rgba() functions
+					hsl_fn = false, -- CSS hsl() and hsla() functions
+					css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+					css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+					-- Available modes for `mode`: foreground, background,  virtualtext
+					mode = "background", -- Set the display mode.
+					-- Available methods are false / true / "normal" / "lsp" / "both"
+					-- True is same as normal
+					tailwind = true, -- Enable tailwind colors
+					-- parsers can contain values used in |user_default_options|
+					sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
+					virtualtext = "■",
+					-- update color values even if buffer is not focused
+					-- example use: cmp_menu, cmp_docs
+					always_update = true,
+				},
+			})
+		end,
+	},
+	{
+		"folke/flash.nvim",
+		-- event = "VeryLazy",
+		---@type Flash.Config
+		opts = {
+			modes = {
+				search = {
+					enabled = false,
+				},
+				char = {
+					enabled = false,
+				},
+			},
+		},
+		lazy = true,
     -- stylua: ignore
     keys = {
       { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
@@ -70,21 +95,21 @@ return {
       { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
     },
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  },
-  {
-    "barrett-ruth/live-server.nvim",
-    build = "npm i --global live-server",
-    cmd = { "LiveServerStart", "LiveServerStop" },
-    config = true,
-    lazy = true,
-  },
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	{
+		"barrett-ruth/live-server.nvim",
+		build = "npm i --global live-server",
+		cmd = { "LiveServerStart", "LiveServerStop" },
+		config = true,
+		lazy = true,
+	},
 }
