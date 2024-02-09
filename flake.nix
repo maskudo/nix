f4bb1b398516fb49849ea226450ab49d6a9b930f
maskudo <manujkarki101@gmail.com>
2024-02-09 23:34:32 +0545

    feat: full nixos  config

--- flake.nix
+++ flake.nix
@@ -5,32 +5,47 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
   };
 
-  outputs = inputs@{ self, flake-parts, home-manager, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: { };
-      flake = {
-        homeConfigurations =
-          let
-            homeManagerModule = import ./module/home-manager.nix {
-              homeDirectory = /home/mk489;
-              username = "mk489";
-            };
-            homeManager = system:
-              home-manager.lib.homeManagerConfiguration {
-                modules = [ homeManagerModule ];
-                pkgs = nixpkgs.legacyPackages.${system};
-              };
-          in
-          {
-            aarch64-darwin = homeManager "aarch64-darwin";
-            aarch64-linux = homeManager "aarch64-linux";
-            x86_64-darwin = homeManager "x86_64-darwin";
-            x86_64-linux = homeManager "x86_64-linux";
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        system = "x86_64-linux";
+        # > Our main nixos configuration file <
+        modules = [./nixos/configuration.nix];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations =
+      let
+        homeManagerModule = import ./module/home-manager.nix {
+          homeDirectory = /home/mk489;
+          username = "mk489";
+        };
+        homeManager = system:
+          home-manager.lib.homeManagerConfiguration {
+            modules = [ homeManagerModule ];
+            pkgs = nixpkgs.legacyPackages.${system};
           };
+      in
+      {
+        aarch64-darwin = homeManager "aarch64-darwin";
+        aarch64-linux = homeManager "aarch64-linux";
+        x86_64-darwin = homeManager "x86_64-darwin";
+        x86_64-linux = homeManager "x86_64-linux";
       };
     };
 }
