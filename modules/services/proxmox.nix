7e44891e3aff11ac208dcba8ccbba13899f86830
maskudo <manujkarki101@gmail.com>
2025-02-23 17:25:32 +0545

    feat: disable firewall and add static ips

--- modules/services/proxmox.nix
+++ modules/services/proxmox.nix
@@ -22,13 +22,13 @@ in
   config = lib.mkIf config.services.proxmox.enable {
     services.proxmox-ve = {
       enable = true;
-      ipAddress = "192.168.1.146";
+      ipAddress = "192.168.1.67";
     };
     nixpkgs.overlays = [
       proxmox-nixos.overlays.${system}
     ];
 
-    networking.firewall.enable = true;
+    networking.firewall.enable = false;
     networking.firewall.allowedTCPPorts = [
       8006
     ];
@@ -36,6 +36,14 @@ in
       "vmbr0" = {
         interfaces = [ "enp4s0f1" ];
       };
+    };
+    networking.interfaces.vmbr0 = {
+      ipv4.addresses = [
+        {
+          address = "192.168.1.67";
+          prefixLength = 24;
+        }
+      ];
     };
     networking.interfaces.vmbr0.useDHCP = lib.mkDefault true;
     networking.interfaces.enp4s0f1.useDHCP = lib.mkDefault false;
