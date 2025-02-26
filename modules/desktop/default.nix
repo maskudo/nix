41f57fa650cb6903b757be4f9de42e49d1ad2f39
maskudo <manujkarki101@gmail.com>
2025-02-27 02:10:28 +0545

    feat: hyprland and i3wm options, remove xfce, refactor a bit

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     ./i3.nix
     ./hypr.nix
+    ./common.nix
+    ./sddm.nix
   ];
+
+  services.displayManager.defaultSession = lib.mkDefault "none+i3";
+  desktop.i3.enable = lib.mkDefault true;
+  desktop.hyprland.enable = lib.mkDefault false;
 }
