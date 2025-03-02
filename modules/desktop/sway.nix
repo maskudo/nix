1c6917e0bef85f3416635233484de26427c6a23c
maskudo <manujkarki101@gmail.com>
2025-03-02 12:46:05 +0545

    feat: sway

---
+++ modules/desktop/sway.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  options = {
+    desktop.sway.enable = lib.mkOption {
+      description = "Enables sway";
+      default = false;
+      type = lib.types.bool;
+    };
+  };
+
+  config = lib.mkIf config.desktop.sway.enable {
+    programs.sway = {
+      enable = true;
+      xwayland.enable = true;
+      package = pkgs.swayfx;
+      extraOptions = [ "--unsupported-gpu" ];
+    };
+
+    environment.systemPackages = with pkgs; [
+      waybar
+      hyprlock
+      rofi-wayland
+      bemoji
+      swww
+      wl-clipboard
+      wl-clipboard-x11
+      wlr-randr
+      swaynotificationcenter
+    ];
+  };
+}
