c7a5df6980b302d33e578602360cb34b38d6af97
maskudo <manujkarki101@gmail.com>
2025-05-25 18:44:34 +0545

    feat:(aspire) nvidia and no gui apps

--- home/gui.nix
+++ home/gui.nix
@@ -1,64 +1,74 @@
 {
+  lib,
+  config,
   pkgs,
   inputs,
   system,
   ...
 }:
 {
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-25.9.0"
-  ];
+  options = {
+    home.guiApps.enable = lib.mkOption {
+      description = "Enables GUI Apps";
+      default = true;
+      type = lib.types.bool;
+    };
+  };
+  config = lib.mkIf config.home.guiApps.enable {
+    nixpkgs.config.permittedInsecurePackages = [
+      "electron-25.9.0"
+    ];
+    catppuccin.flavor = "mocha";
+    catppuccin.gtk.enable = true;
+    catppuccin.hyprland.enable = true;
 
-  catppuccin.flavor = "mocha";
-  catppuccin.gtk.enable = true;
-  catppuccin.hyprland.enable = true;
+    home.packages = with pkgs; [
+      arandr
+      arc-theme
+      arc-icon-theme
+      betterlockscreen
+      brave
+      catppuccin
+      copyq
+      cozy
+      dex
+      (discord.override {
+        withVencord = true;
+      })
+      deadbeef-with-plugins
+      dracula-theme
+      kanagawa-gtk-theme
+      kanagawa-icon-theme
+      feh
+      (flameshot.override { enableWlrSupport = true; })
+      # firefox
+      gparted
+      kitty
+      libsForQt5.breeze-qt5
+      libresprite
+      lxappearance
+      lxrandr
+      mpv
+      nicotine-plus
+      normcap
+      obs-studio
+      obsidian
+      pinta
+      qbittorrent
+      qbittorrent-nox
+      rxvt-unicode
+      smplayer
+      uget
+      vlc
+      vscode
+      xarchiver
+      xclip
+      xfce.xfconf
+      xfce.thunar
+      xsel
+      inputs.zen-browser.packages."${system}".default
 
-  home.packages = with pkgs; [
-    arandr
-    arc-theme
-    arc-icon-theme
-    betterlockscreen
-    brave
-    catppuccin
-    copyq
-    cozy
-    dex
-    (discord.override {
-      withVencord = true;
-    })
-    deadbeef-with-plugins
-    dracula-theme
-    gromit-mpx
-    kanagawa-gtk-theme
-    kanagawa-icon-theme
-    feh
-    (flameshot.override { enableWlrSupport = true; })
-    # firefox
-    gparted
-    kitty
-    libsForQt5.breeze-qt5
-    libresprite
-    lxappearance
-    lxrandr
-    mpv
-    nicotine-plus
-    normcap
-    obs-studio
-    obsidian
-    pinta
-    qbittorrent
-    qbittorrent-nox
-    rxvt-unicode
-    smplayer
-    uget
-    vlc
-    vscode
-    xarchiver
-    xclip
-    xfce.xfconf
-    xfce.thunar
-    xsel
-    inputs.zen-browser.packages."${system}".default
+    ];
+  };
 
-  ];
 }
